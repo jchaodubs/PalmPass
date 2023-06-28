aafa554cab32c6f893d07a08db05b68fe9c33fab
Jeff Chao <jeffreychao70@gmail.com>
2023-06-28 13:19:38 -0700

    Functional

--- FingCount.py
+++ FingCount.py
@@ -21,16 +21,41 @@ for imgPath in myList:
 
 assert(len(overlayList)==6)#number of images
 
-detector = htm.handDetector(detectionCon = 0.75)
+detector = htm.handDetector(detectionCon = 1)
+
+tipIds=[4,8,12,16,20]#thumb,index,middle,ring,pinky
 
 while True:
 
     success, img = cap.read()
     img = detector.findHands(img)
+    numList = detector.findPosition(img,draw=False)
+    #print(numList)
+    if len(numList)!=0:
+        fingers=[]
+
+        #thumb
+        if numList[tipIds[0]][1]<numList[tipIds[0]-1][1]:#index finger up, more minus = more dramatic, less = small
+                fingers.append(1)
+        else:
+                fingers.append(0)
+        
+        #rest of hand
+        for id in range(1,5):
+            if numList[tipIds[id]][2]<numList[tipIds[id]-2][2]:#index finger up, more minus = more dramatic, less = small
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        #print(fingers)
+        totalFingers=fingers.count(1)
+        print(totalFingers)
+            
     
-    #display hand image
-    h,w,c = overlayList[1].shape
-    img[0:h,0:w] = overlayList[1]
+    #   display hand image
+        h,w,c = overlayList[totalFingers-1].shape
+        img[0:h,0:w] = overlayList[totalFingers-1]
+
+        
 
     cv2.imshow("Image",img)
     cv2.waitKey(1)
